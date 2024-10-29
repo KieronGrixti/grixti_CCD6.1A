77ec45cdc53aa6391c4e86e5275a06965f333e4f
KieronGrixti <kierongrixti@gmail.com>
2024-10-29 08:12:25 +0100

    VirtualPet

---
+++ VirtualPet/Assets/Pet.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pet
+{
+    public string name;
+    public int age;
+    public int happinessLevel;
+    public int energyLevel;
+
+    public void feed()
+    {
+        energyLevel += 1;
+        happinessLevel += 1;
+    }
+
+    public void play()
+    {
+        happinessLevel += 1;
+        energyLevel -= 1;
+    }
+
+    public void sleep()
+    {
+        energyLevel += 1;
+    }
+}
